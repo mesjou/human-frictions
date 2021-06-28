d58a7c77bab8cb47f2e8d07c79e735714b7b49a2
matthias-hettich <matthias.hettich@outlook.de>
2021-06-28 10:08:19 +0200

    added simple training file

--- src/train.py
+++ src/train.py
@@ -1,2 +1,22 @@
+from environment.base_env import BaseEnv
+
+n_steps = 100000
+
+
+def act(ob):
+    return env.action_space.sample()
+
+
 if __name__ == "__main__":
-    pass
+    env = BaseEnv(2)
+    ob = env.reset()
+
+    for step in range(n_steps):
+        actions = {}
+        for idx in range(env.n_agents):
+            actions[idx] = act(ob)
+        ob_next, r, done, _ = env.step(actions)
+        if done:
+            ob = env.reset()
+        else:
+            ob = ob_next
