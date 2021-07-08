1e6efa6d9ad73e555d2850dab9b21dc9eb0389e8
matthias-hettich <matthias.hettich@outlook.de>
2021-07-08 19:22:19 +0200

    Changed the formulation to absolute values of real consumption

--- src/utils/rewards.py
+++ src/utils/rewards.py
@@ -3,5 +3,5 @@ import numpy as np
 
 def utility(labor: float, consumption: float):
     assert labor >= 0, "Labor must not be negative"
-    assert consumption >= 0, "Labor must not be negative"
+    assert consumption >= 0, "Consumption must not be negative"
     return np.log(consumption) - labor
