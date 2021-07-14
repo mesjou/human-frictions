3671273be1df9a9cc946c4bf9c0ecd5881f0b127
AnnaAlmosova <anna@anna.com>
2021-07-14 22:21:22 +0200

    add plotting of training progressg

---
+++ src/utils/plot_progress.py
@@ -0,0 +1,24 @@
+import argparse
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser()
+parser.add_argument("path", default=".")
+
+def plot_rewards(path):
+    """Plots the min, max and mean rewards every 50th episode
+        Inputs:
+            path: str, path to the events file
+        Outputs:
+            plt.fig
+
+    Alternatively run tensorborad --logdir path
+    """
+    rewards = pd.read_csv(os.path.join(path,'progress.csv'), usecols=[0, 1, 2])
+    rewards.iloc[::50,:].plot(xlabel="Episode", ylabel="Reward")
+    plt.show()
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    plot_rewards(args.path)
