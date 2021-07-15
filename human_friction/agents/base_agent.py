99fe325163dffe4a02fd43a2b8871f9745178050
matthias-hettich <matthias.hettich@outlook.de>
2021-07-15 10:05:52 +0200

    seperated base env and nk env

--- human_friction/agents/base_agent.py
+++ human_friction/agents/base_agent.py
@@ -1,8 +1,10 @@
 class Agent(object):
     def __init__(self, agent_id: str):
-        """Superclass for all agents.
+        """
+        Superclass for all agents.
 
-        :param agent_id: (str) a unique id allowing the map to identify the agents
+        Args
+            agent_id (str): a unique id allowing the map to identify the agents
         """
         assert isinstance(agent_id, str)
         self.agent_id = agent_id
