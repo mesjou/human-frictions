b2a347dfe063af41fd862051ca487c6a663b0f05
matthias-hettich <matthias.hettich@outlook.de>
2021-08-16 16:47:40 +0200

    finished callbacks

--- human_friction/rllib/callbacks.py
+++ human_friction/rllib/callbacks.py
@@ -1,5 +1,6 @@
 from typing import Sequence
 
+import numpy as np
 import pandas as pd
 from human_friction.rllib.rllib_env import RllibEnv
 from ray.rllib.agents.callbacks import DefaultCallbacks
@@ -8,10 +9,19 @@ from ray.rllib.evaluation import MultiAgentEpisode, RolloutWorker
 
 
 class MyCallbacks(DefaultCallbacks):
+    def on_episode_start(self, worker: RolloutWorker, base_env: BaseEnv, episode: MultiAgentEpisode, **kwargs):
+        envs: Sequence[RllibEnv] = base_env.get_unwrapped()
+        social_metrics = pd.DataFrame([e.env.scenario_metrics() for e in envs]).mean().to_dict()
+        for k, _ in social_metrics.items():
+            episode.user_data[k] = []
+
     def on_episode_step(self, worker: RolloutWorker, base_env: BaseEnv, episode: MultiAgentEpisode, **kwargs):
         envs: Sequence[RllibEnv] = base_env.get_unwrapped()
         social_metrics = pd.DataFrame([e.env.scenario_metrics() for e in envs]).mean().to_dict()
-
         for k, v in social_metrics.items():
-            episode.custom_metrics[k] = v
-        # print(episode.hist_stats)
+            episode.user_data[k].append(v)
+
+    def on_episode_end(self, worker: RolloutWorker, base_env: BaseEnv, episode: MultiAgentEpisode, **kwargs):
+        for k, v in episode.user_data.items():
+            average_value = np.mean(episode.user_data[k])
+            episode.custom_metrics[k] = average_value
