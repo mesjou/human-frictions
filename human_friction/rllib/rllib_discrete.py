8449bc5f6ca449cfd46cc49ebade7e5014279991
matthias-hettich <matthias.hettich@outlook.de>
2021-08-19 15:42:32 +0200

    separate observations and action_masking

--- human_friction/rllib/rllib_discrete.py
+++ human_friction/rllib/rllib_discrete.py
@@ -5,48 +5,49 @@ from ray.rllib import MultiAgentEnv
 from ray.rllib.utils.typing import MultiAgentDict
 
 OBS_SPACE_AGENT = spaces.Dict(
-    {
-        "average_wage_increase": spaces.Box(0.0, 0.1, shape=(1,), dtype=np.float32),
-        "average_consumption": spaces.Box(0.0, np.inf, shape=(1,), dtype=np.float32),
-        "budget": spaces.Box(-np.inf, np.inf, shape=(1,), dtype=np.float32),
-        "inflation": spaces.Box(-np.inf, np.inf, shape=(1,), dtype=np.float32),
-        "employed_hours": spaces.Box(0.0, 1.0, shape=(1,), dtype=np.float32),
-        "interest": spaces.Box(0.0, np.inf, shape=(1,), dtype=np.float32),
-        "unemployment": spaces.Box(0.0, 1.0, shape=(1,), dtype=np.float32),
-        "action_mask": spaces.Box(0.0, 1.0, shape=(50,), dtype=np.float32),
-    }
+    {"action_mask": spaces.Box(0, 1, shape=(50,)), "state": spaces.Box(-np.inf, np.inf, shape=(8,), dtype=np.float32)}
 )
-
-# Actions of the format consumption x%, reservation wage x%
 ACT_SPACE_AGENT = spaces.Discrete(50)
 
 
 class RllibDiscrete(MultiAgentEnv):
     def __init__(self, env_config):
-        self.env = SimpleNewKeynes(env_config)
+        self.wrapped_env = SimpleNewKeynes(env_config)
         self.observation_space = OBS_SPACE_AGENT
         self.action_space = ACT_SPACE_AGENT
 
     def reset(self) -> MultiAgentDict:
-        obs = self.env.reset()
-        obs = {
-            k: {
-                k1: v1 if type(v1) is np.ndarray else np.array([v1])
-                for k1, v1 in v.items()
-                if k1 in OBS_SPACE_AGENT.spaces.keys()
-            }
-            for k, v in obs.items()
-        }
+        wrapped_obs = self.wrapped_env.reset()
+        obs = self._flatten_observations(wrapped_obs)
+
         return obs
 
     def step(self, actions: MultiAgentDict) -> (MultiAgentDict, MultiAgentDict, MultiAgentDict, MultiAgentDict):
-        obs, r, done, info = self.env.step(actions)
-        obs = {
-            k: {
-                k1: v1 if type(v1) is np.ndarray else np.array([v1])
-                for k1, v1 in v.items()
-                if k1 in OBS_SPACE_AGENT.spaces.keys()
-            }
-            for k, v in obs.items()
-        }
+        wrapped_obs, r, done, info = self.wrapped_env.step(actions)
+        obs = self._flatten_observations(wrapped_obs)
+
         return obs, r, done, info
+
+    @staticmethod
+    def _flatten_observations(obs):
+        """
+        Take observations of the form {agent_id: {key1: value1, key2: value2 ... action_mask: [0, 1, 2]}}
+        and transforms it to {agent_id: {state: np.array(value1, value2 ...], action_mask: [0, 1, 2]}}.
+
+        Args:
+            obs (dict): original observation.
+
+        Returns:
+            flatten_obs (dict): observation that separates action mask from other values.
+
+        """
+        flatten_obs = dict()
+        for agent_id, observations in obs.items():
+            flatten_obs[agent_id] = dict()
+            if "action_mask" in observations.keys():
+                flatten_obs[agent_id]["action_mask"] = observations["action_mask"]
+                del observations["action_mask"]
+            # be sure that the observation keys are always at the same position in the numpy array
+            flatten_obs[agent_id]["state"] = np.fromiter(dict(sorted(observations.items())).values(), dtype=np.float32)
+
+        return flatten_obs
---
+++ tests/test_rllib/test_gym_interface_discrete.py
@@ -0,0 +1,76 @@
+import numpy as np
+from human_friction.environment.new_keynes import NewKeynesMarket
+from human_friction.rllib.rllib_discrete import RllibDiscrete
+
+
+def act(env):
+    return {agent.agent_id: env.action_space.sample() for agent in env.wrapped_env.agents.values()}
+
+
+def test_env():
+    config = {"episode_length": 20, "n_agents": 2, "init_budget": 0.0}
+    n_steps = 20
+    rllib_env = RllibDiscrete(config)
+    ob = rllib_env.reset()  # noqa E841
+    for step in range(n_steps):
+        action = act(rllib_env)
+        ob_next, r, done, _ = rllib_env.step(action)
+        if done["__all__"]:
+            ob = rllib_env.reset()  # noqa E841
+        else:
+            ob = ob_next  # noqa E841
+        assert isinstance(r, dict)
+        assert isinstance(done, dict)
+        assert isinstance(ob_next, dict)
+
+
+def test_step():
+    config = {"episode_length": 20, "n_agents": 2, "seed": 1}
+    rllib_env = RllibDiscrete(config)
+    nk_env = NewKeynesMarket(config)
+
+    ob_rllib = rllib_env.reset()
+    ob_nk = nk_env.reset()
+    assert ob_rllib == ob_nk
+
+    for step in range(20):
+        action = act(rllib_env)
+        ob_next_rllib, r_rllib, done_rllib, _ = rllib_env.step(action)
+        ob_next_nk, r_nk, done_nk, _ = nk_env.step(action)
+
+        assert ob_next_rllib == ob_next_nk
+        assert r_rllib == r_nk
+        assert done_rllib == done_nk
+
+
+def test_config():
+    config = {"episode_length": 20, "n_agents": 2, "init_budget": 3.0, "labor_coefficient": 0.5}
+    rllib_env = RllibDiscrete(config)
+    ob = rllib_env.reset()  # noqa E841
+    action = act(rllib_env)
+    ob_next, r, done, _ = rllib_env.step(action)
+    assert rllib_env.wrapped_env.labor_coefficient == 0.5
+    assert rllib_env.wrapped_env.init_budget == 3.0
+
+
+def test_flatten_observation():
+    config = {"episode_length": 20, "n_agents": 2, "init_budget": 3.0, "labor_coefficient": 0.5}
+    rllib_env = RllibDiscrete(config)
+    input_dict = {
+        "agent-0": {"a": 1.0, "b": 2.0, "c": 3.0, "action_mask": np.array([1.0, 0.0, 1.0])},
+        "agent-1": {"b": 20.0, "a": 10.0, "c": 5.0},
+    }
+    output_dict = {
+        "agent-0": {"state": np.array([1.0, 2.0, 3.0]), "action_mask": np.array([1.0, 0.0, 1.0])},
+        "agent-1": {"state": np.array([10.0, 20.0, 5.0])},
+    }
+
+    for agent_id in output_dict.keys():
+        assert compare_exact(rllib_env._flatten_observations(input_dict)[agent_id], output_dict[agent_id])
+
+
+def compare_exact(first, second):
+    """Return whether two dicts of arrays are exactly equal"""
+    if first.keys() != second.keys():
+        return False
+    return all(np.array_equal(first[key], second[key]) for key in first)
