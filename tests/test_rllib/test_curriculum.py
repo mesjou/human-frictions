88b05cd77cc000176d9c125aac1ed62b483e17e5
matthias-hettich <matthias.hettich@outlook.de>
2021-08-31 10:55:56 +0200

    fixed tests

--- tests/test_rllib/test_curriculum.py
+++ tests/test_rllib/test_curriculum.py
@@ -4,7 +4,7 @@ from human_friction.rllib.curriculum import Curriculum
 def test_curriculum_fn():
     train_results = {"episode_reward_mean": 0.0}
     curriculum = Curriculum()
-    assert curriculum.curriculum_fn(train_results, train_results, train_results) == 20.0
+    assert curriculum.curriculum_fn(train_results, train_results, train_results) == 10.0
 
     train_results["episode_reward_mean"] = 710
     assert curriculum.curriculum_fn(train_results, train_results, train_results) == 8.0
